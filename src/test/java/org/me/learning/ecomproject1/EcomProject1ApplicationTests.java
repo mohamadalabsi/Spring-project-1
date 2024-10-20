7bb86346921d526ba01b8754761a8c8920347581
mohamadalabsi <absimoda1@gmail.com>
2024-10-20 17:23:03 +0200

    Stating the ecom project

---
+++ src/test/java/org/me/learning/ecomproject1/EcomProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.learning.ecomproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
