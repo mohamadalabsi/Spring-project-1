94eb7231f42f4b6b0f05b6a016d94256ea205da9
mohamadalabsi <absimoda1@gmail.com>
2024-10-24 19:26:14 +0200

    CORS Error in Spring Boot

--- src/main/java/org/me/learning/ecomproject1/controller/ProductController.java
+++ src/main/java/org/me/learning/ecomproject1/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.me.learning.ecomproject1.model.Product;
 import org.me.learning.ecomproject1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
