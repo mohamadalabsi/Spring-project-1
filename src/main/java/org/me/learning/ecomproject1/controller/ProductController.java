ee9de35dc74f90386c3772618720fdf10bca6482
mohamadalabsi <absimoda1@gmail.com>
2024-10-27 17:13:01 +0100

    Fetching Image

--- src/main/java/org/me/learning/ecomproject1/controller/ProductController.java
+++ src/main/java/org/me/learning/ecomproject1/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.me.learning.ecomproject1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -58,6 +59,17 @@ public class ProductController {
 
     }
 
+    @GetMapping("product/{productId}/image")
+     public ResponseEntity<byte[]> getProductImage(@PathVariable int productId){
+
+
+            Product product = productService.getProdByID(productId);
+            byte[] imageFile=product.getImageData();
+            return  ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
+
+
+
+    }
 
 
 
